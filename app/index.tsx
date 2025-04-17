6f021f58c6ce0a7e1093d195cc4b240242b2a79a
EzequielBurg <ezekiel_sci@hotmail.com>
2025-04-17 16:06:03 -0300

    thema

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,13 @@
 import { Redirect } from "expo-router";
+import { ThemeProvider } from "styled-components";
+
+import theme from "../theme";
 
 export default function App() {
-  return <Redirect href="/Groups/index" />
+  return (
+    <ThemeProvider theme={theme}>
+      <Redirect href="/Groups/index" />
+
+    </ThemeProvider>
+  )
 }
