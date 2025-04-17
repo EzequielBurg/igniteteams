11d5c0eb9cab355d963fd75de92a415b437e4471
EzequielBurg <ezekiel_sci@hotmail.com>
2025-04-17 15:40:59 -0300

    first commit

---
+++ app/Groups/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from 'react-native'
+import { Container, Title } from './styles'
+
+export default function Groups() {
+  return (
+    <Container>
+        <Title>Groups</Title>
+    </Container>
+  )
+}
